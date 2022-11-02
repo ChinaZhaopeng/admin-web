00494a673463771ac3199676406cf5c27d144acb
zhaopeng <zhaopeng@jiuqi.com.cn>
2022-11-02 15:20:01 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import ViewUIPlus from 'view-ui-plus'
+import 'element-plus/dist/index.css'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).use(ViewUIPlus).mount('#app')
